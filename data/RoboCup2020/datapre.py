de2a9b889f29d27db64e06f8c4bc45e3a30b4eda
zqy1000 <55745302+zqy1000@users.noreply.github.com>
2021-05-13 15:31:56 +0800

    Add files via upload

---
+++ data/RoboCup2020/datapre.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import glob, os
+
+# 数据集的位置/home/robocup/Downloads/darknettest/darknet2/data/sipailou1018merge
+imgs_dir = '/home/darknet/data/RoboCup2020'
+print(imgs_dir)
+
+#用作 test 的图片数据的比例
+percentage_test = 10
+
+#创建训练数据集和测试数据集：train.txt 和 test.txt
+file_train = open('train.txt', 'w')
+file_test = open('test.txt', 'w')
+
+counter = 1
+index_test = round(100 / percentage_test)
+for pathAndFilename in glob.iglob(os.path.join(imgs_dir, "*.png")):
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+
+    if counter == index_test:
+        counter = 1
+        file_test.write(imgs_dir + "/" + title + '.png' + "\n")
+    else:
+        file_train.write(imgs_dir + "/" + title + '.png' + "\n")
+        counter = counter + 1
